c8a5ecc744a43db4eb96ddb8a95006c4e277b618
Roman Petrov <rpetrov.ru@gmail.com>
2019-09-19 18:03:20 +0300

    Responsive header menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,21 +10,34 @@
       Nonograms
     </h1>
 
-    <nav v-show="this.$router.currentRoute.name !== 'choose_language'">
-      <router-link :to="{name: 'home'}">{{ $t('header.home') }}</router-link> |
-      <router-link :to="{name: 'about'}">{{ $t('header.about') }}</router-link> |
-      <span class="dropdown">
-        <span class="label">{{ $t('header.language') }}</span>
-        <ul>
-          <li @click="changeLanguage('en')" :class="{active: this.$i18n.locale === 'en'}">English</li>
-          <li @click="changeLanguage('ru')" :class="{active: this.$i18n.locale === 'ru'}">Русский</li>
-        </ul>
-      </span>
+    <nav ref="navbar" v-show="this.$router.currentRoute.name !== 'choose_language'">
+      <span class="burger" @click="menuOpened = !menuOpened" :class="{opened: menuOpened}"><span></span></span>
+
+      <ul :class="{opened: menuOpened}">
+        <li><router-link :to="{name: 'home'}">{{ $t('header.home') }}</router-link></li>
+        <li><router-link :to="{name: 'about'}">{{ $t('header.about') }}</router-link></li>
+        <li>
+          <div class="dropdown">
+            <span class="label">{{ $t('header.language') }}</span>
+            <ul>
+              <li @click="changeLanguage('en')" :class="{active: this.$i18n.locale === 'en'}">English</li>
+              <li @click="changeLanguage('ru')" :class="{active: this.$i18n.locale === 'ru'}">Русский</li>
+            </ul>
+          </div>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
 <script>
+let menuEventListener;
+
 export default {
+  data: function () {
+    return {
+      menuOpened: false
+    };
+  },
   methods: {
     toggleLogoCell(event) {
       event.target.classList.toggle('cancelled');
@@ -46,6 +59,25 @@ export default {
 
       toRoute.params.lang = language;
       this.$router.push(toRoute);
+    }
+  },
+  watch: {
+    /**
+     * Add click listener on document when menu is open to close menu after click out of #navbar element.
+     */
+    menuOpened: function (open) {
+      if (open) {
+        let self = this;
+        menuEventListener = document.addEventListener('click', /** @type MouseEvent */ function (event) {
+          if (!self.$refs.navbar.contains(event.target)) {
+            self.menuOpened = false;
+            document.removeEventListener('click', menuEventListener);
+          }
+        });
+      }
+      else if (menuEventListener) {
+        document.removeEventListener('click', menuEventListener);
+      }
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentLanguage: 'en',
+    currentLanguage: '',
 
     gameData: null,
     gameProcess: [],
